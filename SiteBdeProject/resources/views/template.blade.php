ce5ff12ae3a0579ca0e64886601fc3ab95cd3319
Ssaubeck <33596354+Ssaubeck@users.noreply.github.com>
2019-01-18 09:18:56 +0100

    mise à jour du template

--- SiteBdeProject/resources/views/template.blade.php
+++ SiteBdeProject/resources/views/template.blade.php
@@ -20,7 +20,7 @@
 			</nav> 
             <div  class="navigation">
             <ul>
-                <li><img src="img/cesi-logo.png"></li>
+                <li id="img"><img src="img/cesi-logo.png"></li>
                 <li><a href="/">CESI Centre de Lyon</a></li>
                 <li><a href="/Shop"> Boutique</a></li>
                 <li><a href="/Event"> Evènement </a></li>
