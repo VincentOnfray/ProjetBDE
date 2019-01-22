5f6dc68b9139e8ee907f86da34534a56e8991d6b
Exia-Jellyfish <33414884+Exia-Jellyfish@users.noreply.github.com>
2019-01-22 14:14:12 +0100

    css connexion

--- SiteBdeProject/resources/views/sessions/create.blade.php
+++ SiteBdeProject/resources/views/sessions/create.blade.php
@@ -2,31 +2,34 @@
 
 
 <!-- page de login d'utilisateur-->
-
+@section('css')
+<link rel="stylesheet" type="text/css" href="css/connexion.css">
+@endsection
 @section('content')
 
-<h1> Bon retour parmis nous </h1>
-<form method="POST" action="/login">
+<h1> Bon retour parmis nous !</h1>
+<form id='connexion' method="POST" action="/login">
 	
 	{{csrf_field()}}
 	<div class="form group">
 		<label for="email">Adresse E-Mail:</label>
 		<input type="email" class="form_control" id="email" name="email">
+		{!! $errors->first('email','<p class="help">email invalide</p>') !!}
 	</div>
 
 	<div class="form group">
 		<label for="password">Mot de Passe:</label>
 		<input type="password" class="form_control" id="password" name="password">
+		{!! $errors->first('password','<p class="help">mot de passe invalide</p>') !!}
 	</div>
 
 
-	<div class="form group">
+	<div class="form button">
 		 <button style="cursor:pointer" type="submit" class="btn btn-primary">Login</button>
         </div>
         
 	</div>
-
-	
+</form>
 
 
 @endsection
