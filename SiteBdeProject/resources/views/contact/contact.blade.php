7c55a5fff60720576ed7b8aea083c48bfd9e11ec
Exia-Jellyfish <33414884+Exia-Jellyfish@users.noreply.github.com>
2019-01-25 11:50:05 +0100

    contact css etc

--- SiteBdeProject/resources/views/contact/contact.blade.php
+++ SiteBdeProject/resources/views/contact/contact.blade.php
@@ -8,15 +8,20 @@
 @section ('content')
 <h1>Contacter le BDE</h1>
 
-<h2 id="tel"><a href="/telephonecontact"><i class="fas fa-headset"></i>Téléphone</a></h2>
-<p>Appelez un membre du BDE
-Service & appel gratuit de 9h à 12h et de 14h à 17h du lundi au vendredi.
-La réactivité de réponse des services dépend de la disponibilité de chaque membre du BDE.
-Numéro du Cesi : <em>0 800 054 568</em>
-</p>
+<div class="flex-container">
 
-<h2 id="mail"><a href="/mailcontact"><i class="far fa-envelope"></i>Mail</a></h2>
-<p>Contacter le BDE facilement grâce à une formulaire de contact.
-	<em>Notes</em> : Les membres du BDE regardent souvent leur courrier électronique.</p>
+<div><h2 id="tel"><i class="fas fa-headset"></i>Téléphone</h2>
+<p id="teltext">Vous pouvez appelez un membre du BDE
+<br>Service & appel gratuit de 9h à 12h et de 14h à 17h du lundi au vendredi.
+<br>La réactivité de réponse des services dépend de la disponibilité de chaque membre du BDE.
+<br>Numéro du Cesi : <em>0 800 054 568</em>
+</p>
+</div>
+
+<div><h2 id="mail"><a href="/mailcontact"><i class="far fa-envelope"></i>Mail</a></h2>
+<p id="mailtext">Contacter le BDE facilement grâce à une formulaire de contact.
+	<br><em>Notes</em> : Les membres du BDE regardent souvent leur courrier électronique.</p>
+</div>
+</div>
 
 @endsection
