a15a15bd6b4fbc3c9cfd5e85c5767b8b1a99e645
Ssaubeck <33596354+Ssaubeck@users.noreply.github.com>
2019-01-28 13:45:59 +0100

    boutique sql et css

--- SiteBdeProject/resources/views/boutique/goodies.blade.php
+++ SiteBdeProject/resources/views/boutique/goodies.blade.php
@@ -1,10 +1,17 @@
 @extends("boutique.shop")
+
+@section('css+')
+<link rel="stylesheet" href="../css/shop_content.css" />
+@endsection
 @section('page')
 
 	@foreach($shop as $shop)
-		<h3>{{$shop->nom}}</h3> <br>
-		<p>{{$shop->Prix}}</p><br>
-		<p>{{$shop->Description}}</p><br>
+		<div id="boite">
+			<h3>{{$shop->nom}}</h3> <br>
+			<p>{{$shop->Prix}}</p><br>
+			<p>{{$shop->Description}}</p><br>
+		</div>
+		
 	@endforeach
 
 @endsection
--- SiteBdeProject/resources/views/boutique/home.blade.php
+++ SiteBdeProject/resources/views/boutique/home.blade.php
@@ -1,4 +1,7 @@
 @extends("boutique.shop")
+@section('css+')
+<link rel="stylesheet" href="../css/shop_content.css" />
+@endsection
 @section('page')
 Je suis la page principal de la boutique
 @endsection
