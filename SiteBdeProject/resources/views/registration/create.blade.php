5e9432a61e496a574b49e2b683ceb90aab3ed8df
VincentOnfray <vincent.onfray@viacesi.fr>
2019-01-28 15:33:37 +0100

    Le form de register est beau

--- SiteBdeProject/resources/views/registration/create.blade.php
+++ SiteBdeProject/resources/views/registration/create.blade.php
@@ -9,41 +9,7 @@
 @section('content')
 
 <h1> Rejoins Nous!</h1>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
 
 
 <form id=register method="POST" action="/register">
@@ -112,12 +78,12 @@
 		</select>
 
 	</div>
-	<div class="form group" href="/ppdp">
-			<label for="check" href="/ppdp">J'accepte les conditions d'utilisation du site: </label>
+	<div class="form group check" href="/ppdp">
+			<label for="check" href="/ppdp">J'accepte les conditions d'utilisation du site</label>
 
-			<input type="checkbox" name="check">
+			<input type="checkbox" name="check" class="check">
 			<a class="btn btn-primary" href="/ppdp" role="button">Politique de protection des données</a>
-			<button class="btn btn-primary" type="submit">Button</button>
+		
 			
 	</div>
 
