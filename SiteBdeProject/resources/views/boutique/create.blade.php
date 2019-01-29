ec78b5099555302509546019a077687b06943b7a
Ssaubeck <33596354+Ssaubeck@users.noreply.github.com>
2019-01-29 10:15:12 +0100

    css du formulaire d'ajout d'article

--- SiteBdeProject/resources/views/boutique/create.blade.php
+++ SiteBdeProject/resources/views/boutique/create.blade.php
@@ -1,12 +1,15 @@
 @extends("boutique.shop")
 
+@section('css')
+<link rel="stylesheet" href="css/form.css" />
+@endsection
 
 <!-- page d'enregistrement d'idée-->
 
 @section('page')
 
 <h1> Ajouter un produit</h1>
-<form enctype="multipart/form-data" method="POST" action="/create_item"  > 
+<form class="form " enctype="multipart/form-data" method="POST" action="/create_item"  > 
 
 	
 	{{csrf_field()}}
@@ -14,17 +17,17 @@
 	
 
 
-	<div class="form group">
+	<div class="group">
 		<label for="name">Nom de L'article</label>
 		<input type="text" class="form_control"  id="name" name="name">
 	</div>
 
-	<div class="form group">
+	<div class="group">
 		<label for="description">Description de l'article</label>
 		<input type="text" class="form_control"  name="description">
 	</div>
 
-	<div class="form group">
+	<div class="group">
 			<label for="category">Catégorie</label>
 		<select name="category">
 
@@ -39,13 +42,13 @@
 	
 	</div>
 
-	<div class="form group">
+	<div class="group">
 			<label for="price">Prix (€)</label>
 			<input type="number" class="form_control"  id="price" name="price" step="0.01">
 	</div>
 
-    <div class="custom-file">
-         <label class="custom-file-label" for="image">Image d'Illustration (obligatoire) :</label>
+    <div class="group">
+          <label for="description">Image du Produit: (Obligatoire)</label>
          <input type="file" id="image" name="image" >
     </div>
 
@@ -53,7 +56,7 @@
 
 
 
-	<div class="form group">
+	<div class="group">
 		 <button style="cursor:pointer" type="submit" class="btn btn-primary"  >Ajouter a la boutique</button>
     </div>
         
